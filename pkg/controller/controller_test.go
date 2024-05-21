5a466f9ca95237160041a6d2f906b878db2c06f3
umothadaka <uday.madhav88@gmail.com>
2024-05-21 11:27:44 -0500

    Removed extra line for all go files
    Signed-off-by: umothadaka <uday.madhav88@gmail.com>

--- pkg/controller/controller_test.go
+++ pkg/controller/controller_test.go
@@ -5,7 +5,7 @@ Licensed under the Apache License, Version 2.0 (the "License");
 you may not use this file except in compliance with the License.
 You may obtain a copy of the License at
 
-	http://www.apache.org/licenses/LICENSE-2.0
+    http://www.apache.org/licenses/LICENSE-2.0
 
 Unless required by applicable law or agreed to in writing, software
 distributed under the License is distributed on an "AS IS" BASIS,
@@ -161,7 +161,7 @@ func TestDeepCopy(t *testing.T) {
 	}
 }
 
-// TestZmsGetSignedDomains - test zms API call to get signed domains
+//TestZmsGetSignedDomains - test zms API call to get signed domains
 func TestZmsGetSignedDomains(t *testing.T) {
 	d := getFakeDomain()
 	signedDomain := zms.SignedDomains{
